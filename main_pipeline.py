efd29925fb1acbb511a9c3b056e5a8fc9a589832
Utsav Acharya <91122820+clerisyutsav47@users.noreply.github.com>
2025-05-01 08:52:59 +0545

    fix bugs

--- main_pipeline.py
+++ main_pipeline.py
@@ -1,20 +1,9 @@
-from parsers.parse_docs import (
-    parse_answer_library,
-    parse_frameworks,
-    parse_policy_folder,
-)
-
 from embedder.embedder import create_embeddings, embed_query
 from retriever.retriever import retrieve_relevant_context
 from generator.generator import generate_answer
 
 
-frameworks = parse_frameworks("data/frameworks.xlsx")
-policies = parse_policy_folder("data/policies/")
-# answer_library_entry = parse_answer_library("data/answer_library_entry.xlsx")
-
-
-def run_rag_pipeline(query):
+def run_rag_pipeline(query, policies):
     embeddings = create_embeddings(policies)
     query_emb = embed_query(query)
     context, scores = retrieve_relevant_context(query_emb, policies, embeddings)
